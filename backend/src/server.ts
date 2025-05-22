6b5a86329dfbce11d1029566a2c3011a8a30c4cf
Yuri Severo <ryurisevero@gmail.com>
2025-05-22 08:44:39 -0300

    feat: add the routes to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { routes } from "./routes";
 
 const app = express();
 const port = 3000;
@@ -7,5 +8,7 @@ const port = 3000;
 app.use(cors({ origin: ["*"] }));
 
 app.use(express.json());
+
+app.use("/api", routes)
 
 app.listen(port, () => console.log(`Listening in http://localhost:${port}/api`));
