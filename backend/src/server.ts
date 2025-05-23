8e650b4bd4c75fe2bd56e07d75550a27f52a4443
Adler Castro <adlercastro01@gmail.com>
2025-05-23 10:37:38 -0300

    :recycle: refactor: adpatando para utilizar a constante env tipada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import { env } from "./config/env.config";
 
 const app = express();
-const port = 3000;
 
 app.use(cors({ origin: ["*"] }));
 
 app.use(express.json());
 
-app.use("/api", routes)
+app.use("/api", routes);
 
-app.listen(port, () => console.log(`Listening in http://localhost:${port}/api`));
+app.listen(env.PORT, () =>
+  console.log(`Listening in http://localhost:${env.PORT}/api`)
+);
