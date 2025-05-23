4bbcd0c7c96eee115129bf8c6851c484586d9359
Adler Castro <adlercastro01@gmail.com>
2025-05-23 11:20:41 -0300

    :recycle: refactor: Adaptando para utilizar a env tipada

--- backend/src/database/db.connection.ts
+++ backend/src/database/db.connection.ts
@@ -2,12 +2,13 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema/index";
 import dotenv from "dotenv";
+import { env } from "../config/env.config";
 
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+  connectionString: env.DATABASE_URL,
+  ssl: true,
 });
 
 export const db = drizzle(pool, { schema });
