018c99979b36c70910a26ebf8c94996b2ecda66f
Yuri Severo <ryurisevero@gmail.com>
2025-05-28 09:48:31 -0300

    feat: add user zod schema

---
+++ backend/src/modules/user/user.dto.ts
@@ -0,0 +1,16 @@
+import {z} from "zod"
+
+export const zUserSchema = z.object({
+    role_id: z.string(),
+    department_id: z.string().nullable(),
+    course_id: z.string().nullable(),
+    name: z.string(),
+    password: z.string(),
+    email: z.string().nullable(),
+    cpf: z.string(),
+    registration: z.string().nullable(),
+    phone_number: z.string(),
+    address: z.string(),
+});
+
+export type zUserSchema = z.infer<typeof zUserSchema>;
