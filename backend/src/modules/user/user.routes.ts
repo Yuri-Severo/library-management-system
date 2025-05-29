380cf20280c790aa9e269639ebe26412b5e8c37e
Yuri Severo <ryurisevero@gmail.com>
2025-05-28 21:45:37 -0300

    feat: add user routes

---
+++ backend/src/modules/user/user.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {UserController} from "./user.controller";
+
+const userRouter: Router = Router();
+
+userRouter.get("/users", UserController.getAll.bind(UserController));
+userRouter.get("/user/:id", UserController.getOneById.bind(UserController));
+userRouter.get("/user/cpf/:cpf", UserController.getOneByCpf.bind(UserController));
+userRouter.get("/user/name/:name", UserController.getOneByName.bind(UserController));
+userRouter.get("/user/registration/:registration", UserController.getOneByRegistration.bind(UserController));
+userRouter.put("/user/:id", UserController.update.bind(UserController));
+userRouter.post("/user", UserController.create.bind(UserController));
+userRouter.delete("/user/:id", UserController.delete.bind(UserController));
+
+export { userRouter };
