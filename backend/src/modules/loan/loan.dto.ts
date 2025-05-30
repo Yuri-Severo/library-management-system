e5fefe5609816cf957dfb9313e55d6734bdfd9b7
Yuri Severo <ryurisevero@gmail.com>
2025-05-29 22:06:35 -0300

    feat: add loan dto

---
+++ backend/src/modules/loan/loan.dto.ts
@@ -0,0 +1,12 @@
+import {z} from "zod"
+
+export const zLoanSchema = z.object({
+    book_id: z.string(),
+    user_id: z.string(),
+    loan_date: z.string(),
+    due_date: z.string(),
+    returned_date: z.string().nullable(),
+    fine_amount: z.number().int(),
+    fine_status: z.boolean(),
+    status: z.string(),
+});
