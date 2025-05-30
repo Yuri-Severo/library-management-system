db9cff2e34f68e44e7812f4266e1b3255055d208
Adler Castro <adlercastro01@gmail.com>
2025-05-30 05:05:47 -0300

    :sparkles: feat: enum das páginas da aplicação e divisão por autenticadas e não autenticadas

---
+++ frontend/src/enums/pages.enum.ts
@@ -0,0 +1,16 @@
+export enum PAGES {
+  HOME = '/',
+  LOGIN = '/login',
+  REGISTER = '/register',
+  HOME_AUTH = '/home',
+}
+
+export enum NEED_AUTH {
+  HOME_AUTH = '/home',
+}
+
+export enum NO_AUTH {
+  HOME = '/',
+  LOGIN = '/login',
+  REGISTER = '/register',
+}
