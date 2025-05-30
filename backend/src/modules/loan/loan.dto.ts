121fd4a246b8aedc9cec2fe1454f6aa04361e345
Yuri Severo <ryurisevero@gmail.com>
2025-05-29 23:09:11 -0300

    style(loan.dto): format column names

--- backend/src/modules/loan/loan.dto.ts
+++ backend/src/modules/loan/loan.dto.ts
@@ -1,14 +1,14 @@
-import {z} from "zod"
+import { z } from "zod";
 
 export const zLoanSchema = z.object({
-    book_id: z.string(),
-    user_id: z.string(),
-    loan_date: z.string(),
-    due_date: z.string(),
-    returned_date: z.string().nullable(),
-    fine_amount: z.number().int(),
-    fine_status: z.boolean(),
-    status: z.string(),
+  bookId: z.string(),
+  userId: z.string(),
+  loanDate: z.string(),
+  dueDate: z.string(),
+  returnedDate: z.string().nullable(),
+  fineAmount: z.string(),
+  fineStatus: z.boolean(),
+  status: z.string(),
 });
 
-export type zLoanSchema = z.infer<typeof zLoanSchema>;
+export type zLoanSchemaType = z.infer<typeof zLoanSchema>;
