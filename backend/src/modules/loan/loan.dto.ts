c01ecaf9f4d80435c716945e328c7aea784e961f
Yuri Severo <ryurisevero@gmail.com>
2025-05-30 14:41:58 -0300

    fix(loan.dto): remove unecessary values

--- backend/src/modules/loan/loan.dto.ts
+++ backend/src/modules/loan/loan.dto.ts
@@ -1,14 +1,8 @@
 import { z } from "zod";
 
 export const zLoanSchema = z.object({
-  bookId: z.string(),
-  userId: z.string(),
-  loanDate: z.string(),
-  dueDate: z.string(),
-  returnedDate: z.string().nullable(),
-  fineAmount: z.string(),
-  fineStatus: z.boolean(),
-  status: z.string(),
+  book_id: z.string(),
+  user_id: z.string(),
 });
 
 export type zLoanSchemaType = z.infer<typeof zLoanSchema>;
