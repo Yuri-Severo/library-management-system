97788250fc6eeab72abfdbc4310498dd9002ad3c
Yuri Severo <ryurisevero@gmail.com>
2025-05-29 23:27:39 -0300

    feat(routes): add loanRouter to routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { courseRouter } from "./modules/course/course.routes";
 import { roleRouter } from "./modules/role/role.routes";
 import { departmentRouter } from "./modules/department/department.routes";
 import { userRouter } from "./modules/user/user.routes";
+import { loanRouter } from "./modules/loan/course.routes";
 
 const routes: Router = Router();
 
@@ -10,5 +11,6 @@ routes.use("/course", courseRouter);
 routes.use("/role", roleRouter);
 routes.use("/department", departmentRouter);
 routes.use("/user", userRouter);
+routes.use("/loan",loanRouter)
 
 export { routes };
