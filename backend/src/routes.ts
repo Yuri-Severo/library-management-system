a4ce2c691b81d8f94673b24c1fb962b4fb77a495
Yuri Severo <ryurisevero@gmail.com>
2025-05-30 00:08:58 -0300

    feat: add bookRouter to routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { courseRouter } from "./modules/course/course.routes";
 import { roleRouter } from "./modules/role/role.routes";
 import { departmentRouter } from "./modules/department/department.routes";
 import { userRouter } from "./modules/user/user.routes";
+import { bookRouter } from "./modules/book/book.routes";
 
 const routes: Router = Router();
 
@@ -10,5 +11,6 @@ routes.use("/course", courseRouter);
 routes.use("/role", roleRouter);
 routes.use("/department", departmentRouter);
 routes.use("/user", userRouter);
+routes.use("/book", bookRouter);
 
 export { routes };
