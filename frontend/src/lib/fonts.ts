9f2c9d95b815db9c1a60cfd224ac5888de489cb1
Adler Castro <adlercastro01@gmail.com>
2025-05-29 22:25:56 -0300

    :sparkles: feat: adição das fontes utilizadas no projeto

---
+++ frontend/src/lib/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+});
+
+export const FONTS = {
+  inter,
+};
