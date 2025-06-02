8397bdfd476330cae85cf52c704c01ebbed90ffe
Yuri Severo <ryurisevero@gmail.com>
2025-06-02 09:08:03 -0300

    feat(user.routes): add authMiddleware

--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -1,16 +1,19 @@
-import { Router } from "express";
+import { Router, RequestHandler } from "express";
 import {UserController} from "./user.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const userRouter: Router = Router();
+userRouter.post("/user/register", UserController.register.bind(UserController));
+userRouter.post('/user/login', UserController.login.bind(UserController));
 
+userRouter.use(authMiddleware as RequestHandler); 
+
+userRouter.put("/user/:id", UserController.update.bind(UserController));
 userRouter.get("/users", UserController.getAll.bind(UserController));
 userRouter.get("/user/:id", UserController.getOneById.bind(UserController));
 userRouter.get("/user/cpf/:cpf", UserController.getOneByCpf.bind(UserController));
 userRouter.get("/user/name/:name", UserController.getByName.bind(UserController));
 userRouter.get("/user/registration/:registration", UserController.getOneByRegistration.bind(UserController));
-userRouter.put("/user/:id", UserController.update.bind(UserController));
-userRouter.post("/user/register", UserController.register.bind(UserController));
-userRouter.post('/user/login', UserController.login.bind(UserController));
 userRouter.delete("/user/:id", UserController.delete.bind(UserController));
 
 export { userRouter };
